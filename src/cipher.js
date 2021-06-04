5070a1e93a45034ec55ae50ac4e381b4a633dcb6
Camila Conte <caxconte@gmail.com>
2021-06-04 17:17:27 -0300

    HTML atualizado

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,23 @@
-const cipher = {
-  const alfabeto = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ']
-  
+const cipher = {  
   encode: function (offset, str) {
+    let textMessage = "";
+    // for pro "giro" pra obter valor unicode de cada i da string textMessage
+      for (let i = 0; i < str.length; i++) {
+        let newMessage = str.charCodeAt(i) + offset;
+        while (newMessage = str.charCodeAt(i) >= 65 && crypt <= 90 ) {
+          newMessage = str.charCodeAt(i) - 65 + offset % 26  + 65
+      } newMessage(String.fromCharCode(newMessage)) 
+    } return newMessage;
   }
-  decode: function (offset, str) {
+
+  decode: function (offset, str){ 
+    let textDecrypt = "";
+      
+      for (let i = 0; i < str.length; i++) {
+        let decrypt = str.fromCharCode(i) - offset;
+        while (decrypt = 90) {
+          decrypt = str.fromCharCode(textDecrypt)
+      }
+    } return textDecrypt;
   }
 }
