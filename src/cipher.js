ff873ed3392afe31e0694c9c031c0d06c09f58c0
Camila Conte <caxconte@gmail.com>
2021-06-04 11:35:22 -0300

    first commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,24 @@
 const cipher = {
-  // ...
-};
+  const alfabeto = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ']
+  
+  encode: function (offset, str) {
+  }
+  decode: function (offset, str) {
+  }
+}
+
+((codASC - cod1aLetra + offset) % tamDoAlfabeto) + cod1aLetra
+codASC => cod0a25 => offset => loop => codASC
 
 export default cipher;
+    // cipher.encode(offset, string)
+    //   function myFunction() {
+    //     var str = "HELLO WORLD";
+    //     var n = str.charCodeAt(0);
+    //     document.getElementById("demo").innerHTML = n;
+    // }
+    // cipher.decode(offset, string)
+    
+    // // codigoASC => codigo0a25 => desloco (offset) => loop => codigoASC
+    // // let alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    // // let novaMensagem = ('')
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import cipher from './cipher.js';
 
+const offset = document.getElementById('offset_input').value;
+
+const textMessage = document.getElementById('texto_input')
+
+
+// cipher.encode();
+// cipher.decode();
+
 console.log(cipher);
