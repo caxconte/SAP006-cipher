621641b57d532c52ab051faa8f813aff24993290
Camila Conte <caxconte@gmail.com>
2021-06-14 18:16:44 -0300

    working cypher

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,37 @@
 const cipher = {
-  encode: function (offset, str) {
+  encode: function(offset, string){
     let textEncrypt = "";
-    // for pro "giro" pra obter valor unicode de cada i da string textEncrypt
-    for (let i = 0; i < str.length; i++) {
-      let encrypt = str.charCodeAt[i];
+// Criar função com a fórmula do Daniel
+    function cifra(first, loop, encrypt){
+      let newEncrypt  = ((encrypt - first + offset) % loop) + first;
+      let newChar = String.fromCharCode(newEncrypt);
+      textEncrypt = textEncrypt.concat(newChar);
+    }
+    // FOR pro "giro" pra obter valor unicode de cada i da string textEncrypt
+    for (let i = 0; i < string.length; i++) {
+      let encrypt = string.charCodeAt(i);
       // função if pra descobrir se o valor da string tá
       // entre 65 e 90 (A a Z) e aplicar a fórmula do Daniel
-      if (encrypt >= 65 && crypt <= 90 ) {
-      encrypt = ((str.charCodeAt[i] - 65 + offset) % 26) + 65
-      } textEncrypt += String.fromCharCode(encrypt) 
+      if (encrypt >= 65 && encrypt <= 90) {
+        encrypt = ((encrypt - 65 + offset) % 26) + 65;
+      } textEncrypt += String.fromCharCode(encrypt);
     } return textEncrypt;
-  }
+  },
 
-  decode: function (offset, str){
+  decode: function(offset, string){
     let textDecrypt = "";
-    for (let i = 0; i < str.length; i++) {
-      let decrypt = str.fromCharCode[i] - offset;
+    // Criar função com a fórmula do Daniel INVERSA
+    function decifra(first, loop, decrypt){
+      let newDecrypt  = ((decrypt + first - offset) % loop) + first;
+      let newChar = String.fromCharCode(newDecrypt);
+      textDecrypt = textDecrypt.concat(newChar);
+    }
+    for (let i = 0; i < string.length; i++) {
+      let decrypt = string.charCodeAt(i);
       if (decrypt >= 65 && decrypt <= 90) {
-        // Ver como inverte a fórmula do Daniel
-        decrypt = ((str.charCodeAt(i) - 65 + offset) % 26) + 65
-      } textDecrypt += String.fromCharCode(decrypt)
+        decrypt = ((decrypt + first - offset) % loop) + first;
+      } textDecrypt += String.fromCharCode(decrypt);
     } return textDecrypt;
   }
 }
-
-// FORMULA DO DANIEL
-// ((codASC - cod1aletra + offset) % 26) + cod1aletra
+  export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
 import cipher from './cipher.js';
 
-const offset = document.getElementById("offset_input").value;
-const str = document.getElementById("texto_input");
+let textToEncrypt = document.getElementById("textToEncrypt");
+let textToDecrypt = document.getElementById("textToDecrypt");
+
+// console.log(offset);
+// console.log(str);
 
 // Botões //
 
-const encryptButton = document.getElementById("criptografar");
-    criptografar.addEventListener("click")
-    document.getElementById("retornodafuncao").innerHTML = cipher.encode(offset, str);
+const encryptBtn = document.getElementById("encryptButton");
+const translateBtn = document.getElementById("translateButton");
+
+encryptBtn.onclick = function (event) {
+    let str = textToEncrypt.value;
+    let offsetStr = document.getElementById("offset").value;
+    let offset = parseInt(offsetStr);
+
+    if (offset > 0) {
+        let decryptText = cipher.encode(offset, str);
+        textToDecrypt.value = decryptText;
+    // } else if (offset < 0) {
+    //     let decryptText = cipher.decode((-offset), text);
+    //     textToDecrypt.value = decryptText;
+    }
+}
+
+translateBtn.onclick = function (event) {
+    let str = textToDecrypt.value;
+    let offsetStr = document.getElementById("offset").value;
+    let offset = parseInt(offsetStr);
+
+    if (offset > 0) {
+        let decryptText = cipher.decode(offset,str);
+        textToDecrypt.value = decryptText;
+    // } else if (offset < 0) {
+    //     let decryptText = cipher.decode((-offset), text);
+    //     textToDecrypt.value = decryptText;
+    }
+}
 
 // cipher.encode();
 // cipher.decode();
